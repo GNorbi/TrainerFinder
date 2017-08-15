bf4880da4e9c420f2882ffada796976cc8c413e9
Norbert Galgóczi <minipek.gn@gmail.com>
2017-08-15 19:13:49 +0200

    model repair

--- trainers/admin.py
+++ trainers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import SportPlace, SportPlaceInstance, SportCourse, SportCourseInstance
+
+admin.site.register(SportPlace)
+admin.site.register(SportPlaceInstance)
+admin.site.register(SportCourse)
+admin.site.register(SportCourseInstance)
+
 
 # Register your models here.
--- trainers/models.py
+++ trainers/models.py
@@ -4,15 +4,31 @@ from datetime import datetime
 class SportPlace:
 	id = models.AutoField(primary_key=True)
 	add_Date = models.DateTimeField(default=datetime.utcnow, blank = True)
-	add_User = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
-
-	typeOf = models.CharField(max_length=1, choices=type, blank=True, default='f', help_text='Hiba Allapot')
-
-	type = (
-		('g', 'Gym'),
-		('h', "2."),
+	add_User = models.ForeignKey('SportPlaceInstance', on_delete=models.SET_NULL, null=True) #TODO USER
+	
+	ty = (
+		('a', "Aerobic Center"),
+		('y', "Yoga Center"),
+		('d', "Dance Center"),
+		('p', "Pilates Center"),
+		('g', "Gym"),
+		('f', "Fitness Center"),
+		('t', "Athletic Center"),
+		('w', "Wellnes Center"),
     	)
 
+	typeOf = models.CharField(max_length=1, choices=ty, blank=True, default='a', help_text='Type')
+
+
+	class Meta:
+		verbose_name = "Sport Place"
+		verbose_name_plural = "Sport Place"	
+
+	def __str__(self):
+		"""
+		String for representing the Model object.
+		"""
+		return str(self.id).encode('UTF-8')
 
 	def save(self, force_insert=False, force_update=False, *args, **kwargs):
 		super(SportPlace, self).save(force_insert, force_update, *args, **kwargs)
@@ -20,67 +36,45 @@ class SportPlace:
 
 class SportPlaceInstance:
 	id = models.AutoField(primary_key=True)
-	sportCourse = models.ForeignKey('SportPlace',on_delete=models.SET_NULL, null = True)
-
+	sportPlace = models.ForeignKey('SportPlace',on_delete=models.SET_NULL, null = True)
+	latitude = models.DecimalField(max_digits=9, decimal_places=6)
+	longitude = models.DecimalField(max_digits=9, decimal_places=6)
 
 
 class SportCourse:
-	id = models.AutoField(primery_key=True)
+	id = models.AutoField(primary_key=True)
 
-	typeOf = models.CharField(max_length=1, choices=type, blank=True, default='f', help_text='Hiba Allapot')
-
-	type = (
-		('g', 'Gym'),
+	type1 = (
+		('a', 'Aerobic Cen'),
 		('h', "2."),
     	)
+
+	typeOf = models.CharField(max_length=1, choices=type1, blank=True, default='f', help_text='Hiba Allapot')
+
+
+
 
 
 class SportCourseInstance:
 	id = models.AutoField(primary_key=True)
-	sportCourse = models.ForeignKey(SportCourse,on_delete=models.SET_NULL, null = True)
+	sportCourse = models.ForeignKey('SportCourse',on_delete=models.SET_NULL, null = True)
+	sportPlace = models.ForeignKey('SportPlaceInstance', on_delete=models.SET_NULL, null = True)
+	coach = models.ForeignKey('SportPlaceInstance', on_delete=models.SET_NULL, null=True) #TODO change to USER
 
 	#egy bizonyos időpontban tartott edzés
-	oneTimeEvent = models.ForeignKey(OneTimeEvent, on_delete=models.SET_NULL, null=True)
+	oneTimeEvent = models.ForeignKey('OneTimeEvent', on_delete=models.SET_NULL, null=True)
 
 	#rendszeresen tartott edzés
-	regularyEvent = models.ForeignKey(RegularyEvent, on_delete=models.SET_NULL, null=True)
+	regularyEvent = models.ForeignKey('RegularyEvent', on_delete=models.SET_NULL, null=True)
 
-
-	coach = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
 
 class RegularyEvent:
-	day1 = models.BooleanField(initial=False)
-	startTime1 = models.TimeField(_(u"Conversation Time"), blank=True)
-	endTime1 = models.TimeField(_(u"Conversation Time"), blank=True)
-
-	day2 = models.BooleanField(initial=False)
-	startTime2 = models.TimeField(_(u"Conversation Time"), blank=True)
-	endTime2 = models.TimeField(_(u"Conversation Time"), blank=True)
-	
-	day3 = models.BooleanField(initial=False)
-	startTime3 = models.TimeField(_(u"Conversation Time"), blank=True)
-	endTime3 = models.TimeField(_(u"Conversation Time"), blank=True)
-	
-	day4 = models.BooleanField(initial=False)
-	startTime4 = models.TimeField(_(u"Conversation Time"), blank=True)
-	endTime4 = models.TimeField(_(u"Conversation Time"), blank=True)
-	
-	day5 = models.BooleanField(initial=False)
-	startTime5 = models.TimeField(_(u"Conversation Time"), blank=True)
-	endTime5 = models.TimeField(_(u"Conversation Time"), blank=True)
-	
-	day6 = models.BooleanField(initial=False)
-	startTime6 = models.TimeField(_(u"Conversation Time"), blank=True)
-	endTime6 = models.TimeField(_(u"Conversation Time"), blank=True)
-	
-	day7 = models.BooleanField(initial=False)
-	startTime7 = models.TimeField(_(u"Conversation Time"), blank=True)
-	endTime7 = models.TimeField(_(u"Conversation Time"), blank=True)
+	startDate = models.DateTimeField(default=datetime.utcnow, blank=True)
+	endDate = models.DateTimeField(default=datetime.utcnow, blank=True)
 
 class OneTimeEvent:
 
 	startDate = models.DateTimeField(default=datetime.utcnow, blank=True)
 	endDate = models.DateTimeField(default=datetime.utcnow, blank=True)
 
-# Create your models here.
